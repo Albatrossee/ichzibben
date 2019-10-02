0f1679d0642dd1802cdd3e6344da9f24874bd21a
unknown <study4die@gmail.com>
2019-10-02 13:13:10 -0700

    First

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import telebot
 
-TOKEN = '736252052:AAHU4iBU51UpCmjAuV16IVty8vM2bTzL3jU'
+TOKEN = '827583705:AAEb2ApHsxNKfuFWtR2Yu_YbR2PMBjmqO3M'
 bot = telebot.TeleBot(TOKEN)
 value = 0
-
-
 
 
 @bot.message_handler(commands=['start'])
@@ -14,18 +12,18 @@ def start_command(message):
 
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Обмен XLM', callback_data='get-XLM')
+        telebot.types.InlineKeyboardButton('Exchange XLM', callback_data='get-XLM')
     )
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Обратная связь', callback_data='get-Call-back')
+        telebot.types.InlineKeyboardButton('Support', callback_data='get-Call-back')
     )
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Проверить заявку по номеру', callback_data='get-check-by-number')
+        telebot.types.InlineKeyboardButton('Check status by number', callback_data='get-check-by-number')
     )
 
     bot.send_message(
         message.chat.id,
-        'Вас приветствует XLM STELLAR EXCHANGE BOT!',
+        'Welcome to XLM STELLAR EXCHANGE BOT!',
         reply_markup=keyboard
     )
 
@@ -42,18 +40,30 @@ def iq_callback(query):
     if data.startswith('getBack'):
         bot.answer_callback_query(query.id)
         start_command(query.message)
-    if data.startswith('getUAH'):
+    if data.startswith('getUSD'):
         bot.answer_callback_query(query.id)
-        gettingUAH(query.message)
-    if data.startswith('getRUB'):
+        gettingUSD(query.message)
+    if data.startswith('getEUR'):
         bot.answer_callback_query(query.id)
-        gettingRUB(query.message)
+        gettingEUR(query.message)
+    if data.startswith('getPPUSD'):
+        bot.answer_callback_query(query.id)
+        gettingPPUSD(query.message)
+    if data.startswith('getppeuro'):
+        bot.answer_callback_query(query.id)
+        gettingPPEUR(query.message)
     if data.startswith('getBackToXLM'):
         bot.answer_callback_query(query.id)
         getMyXLM(query.message)
     if data.startswith('yesButton'):
         bot.answer_callback_query(query.id)
         yesButton(query.message)
+    if data.startswith('yespp'):
+        bot.answer_callback_query(query.id)
+        yesButtonPP(query.message)
+    if data.startswith('yesppeuro'):
+        bot.answer_callback_query(query.id)
+        yesButtonPP(query.message)
     if data.startswith('confirm'):
         bot.answer_callback_query(query.id)
         confirm(query.message)
@@ -72,25 +82,25 @@ def callback(message):
     bot.delete_message(message.chat.id, message.message_id)
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBack')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBack')
     )
-    bot.send_message(message.chat.id, "Оператор\n" + '@StellarExchangeSup', reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Contact\n" + '@StellarExchangeSup', reply_markup=keyboard)
 
 
 def checkByNumber(message):
     bot.delete_message(message.chat.id, message.message_id)
-    bot.send_message(message.chat.id, "Что бы проверить заявку введите: \n" + 'Номер заявки:\n')
+    bot.send_message(message.chat.id, "Enter: \n" + 'Exchange request number:\n')
     bot.register_next_step_handler(message, cheking)
 
 
 def cheking(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBack')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBack')
     )
     bot.delete_message(message.chat.id, message.message_id - 1)
     bot.delete_message(message.chat.id, message.message_id)
-    bot.send_message(message.chat.id, 'Заявка №' + message.text + ' обрабатывается.', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Request №' + message.text + ' is being processed.', reply_markup=keyboard)
 
 
 # Initialization
@@ -100,61 +110,98 @@ def getMyXLM(message):
     bot.send_chat_action(message.chat.id, 'typing')
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('XLM на карту UAH', callback_data='getUAH')
+        telebot.types.InlineKeyboardButton('XLM on USD credit card', callback_data='getUSD'),
+        telebot.types.InlineKeyboardButton('XLM on EUR credit card', callback_data='getEUR')
     )
     keyboard.row(
-        telebot.types.InlineKeyboardButton('XLM на карту RUB', callback_data='getRUB')
+        telebot.types.InlineKeyboardButton('XLM on PayPal', callback_data='getPPUSD'),
     )
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBack')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBack')
     )
 
     bot.send_message(
         message.chat.id,
-        'Выберите способ обмена\n' +
-        '1 XLM -> 2.85 UAH\n' +
-        '1 XLM -> 5.76 RUB',
+        'Choose an exchange method\n' +
+        '1 XLM -> 0.11 USD\n' +
+        '1 XLM -> 0.1 EUR\n',
         reply_markup=keyboard
     )
 
 
-def gettingUAH(message):
+def gettingUSD(message):
     print(message.message_id)
     bot.delete_message(message.chat.id, message.message_id)
     bot.send_chat_action(message.chat.id, 'typing')
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBackToXLM')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
     )
 
     bot.send_message(
         message.chat.id,
-        'Введите количество XLM\n' +
-        'Курс: 2.85',
+        'Enter the amount of XLM\n' +
+        'Rate: 0.11$',
         reply_markup=keyboard
     )
-    bot.register_next_step_handler(message, valueXLMUAH)
+    bot.register_next_step_handler(message, valueXLMUSD)
 
 
-def gettingRUB(message):
+def gettingEUR(message):
+    print(message.message_id)
     bot.delete_message(message.chat.id, message.message_id)
     bot.send_chat_action(message.chat.id, 'typing')
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBackToXLM')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
     )
 
     bot.send_message(
         message.chat.id,
-        'Введите количество XLM\n' +
-        'Курс: 5.76',
+        'Enter the amount of XLM\n' +
+        'Rate: 0.1€',
         reply_markup=keyboard
     )
-    bot.register_next_step_handler(message, valueXLMRUB)
+    bot.register_next_step_handler(message, valueXLMEUR)
+
+
+def gettingPPUSD(message):
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_chat_action(message.chat.id, 'typing')
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
+    )
+
+    bot.send_message(
+        message.chat.id,
+        'Enter the amount of XLM\n' +
+        'Rate: USD = 0.11$ , EUR = 0.1€',
+        reply_markup=keyboard
+    )
+    bot.register_next_step_handler(message, valueXLMPPUSD)
+
+
+def gettingPPEUR(message):
+    print('zbs')
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_chat_action(message.chat.id, 'typing')
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
+    )
+
+    bot.send_message(
+        message.chat.id,
+        'Enter the amount of XLM\n' +
+        'Rate: 0.1€',
+        reply_markup=keyboard
+    )
+    bot.register_next_step_handler(message, valueXLMPPEUR)
 
 
 @bot.message_handler(content_types=['text'])
-def valueXLMUAH(message):
+def valueXLMUSD(message):
     global price
     bot.delete_message(message.chat.id, message.message_id - 1)
     price = message.text
@@ -165,23 +212,49 @@ def valueXLMUAH(message):
             if float(message.text) >= 549:
                 keyboard = telebot.types.InlineKeyboardMarkup()
                 keyboard.row(
-                    telebot.types.InlineKeyboardButton('Да', callback_data='yesButton')
+                    telebot.types.InlineKeyboardButton('Yes', callback_data='yesButton')
                 )
                 keyboard.row(
-                    telebot.types.InlineKeyboardButton('Назад', callback_data='getBackToXLM')
+                    telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
                 )
                 bot.send_message(
                     message.from_user.id,
-                    "Вы хотите обменять " + str(message.text) + "XLM на " + str(float(message.text) * 2.85) + " UAH",
+                    "You want to exchange " + str(message.text) + "XLM to " + str(float(message.text) * 0.11) + " USD",
                     reply_markup=keyboard
                 )
             else:
                 bot.send_message(message.from_user.id, 'min 549')
         except Exception:
-            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+            bot.send_message(message.from_user.id, 'Use numbers')
+
+def valueXLMEUR(message):
+    global price
+    bot.delete_message(message.chat.id, message.message_id - 1)
+    price = message.text
+    global value
+    if value == 0:
+        try:
+
+            if float(message.text) >= 549:
+                keyboard = telebot.types.InlineKeyboardMarkup()
+                keyboard.row(
+                    telebot.types.InlineKeyboardButton('Yes', callback_data='yesButton')
+                )
+                keyboard.row(
+                    telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
+                )
+                bot.send_message(
+                    message.from_user.id,
+                    "You want to exchange " + str(message.text) + "XLM to " + str(float(message.text) * 0.1) + " EUR",
+                    reply_markup=keyboard
+                )
+            else:
+                bot.send_message(message.from_user.id, 'min 549')
+        except Exception:
+            bot.send_message(message.from_user.id, 'Use numbers')
 
 
-def valueXLMRUB(message):
+def valueXLMPPUSD(message):
     global price
     bot.delete_message(message.chat.id, message.message_id - 1)
     price = message.text
@@ -190,42 +263,54 @@ def valueXLMRUB(message):
         try:
             if int(message.text) >= 549:
                 keyboard = telebot.types.InlineKeyboardMarkup()
+                price = message.text
                 keyboard.row(
-                    telebot.types.InlineKeyboardButton('Да', callback_data='yesButton')
+                    telebot.types.InlineKeyboardButton('Yes', callback_data='yespp')
                 )
                 keyboard.row(
-                    telebot.types.InlineKeyboardButton('Назад', callback_data='getBackToXLM')
+                    telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
                 )
                 bot.send_message(
                     message.from_user.id,
-                    "Вы хотите обменять " + str(message.text) + "XLM на " + str(float(message.text) * 5.76) + " RUB",
+                    "You want to exchange " + str(message.text) + "XLM to " + str(float(message.text) * 0.11) + " USD or " + str(float(message.text) * 0.1) + ' EUR',
                     reply_markup=keyboard
                 )
             else:
-                bot.send_message(message.from_user.id, 'min 549т   '
-                                                       ''
-                                                       '')
+                bot.send_message(message.from_user.id, 'min 549')
         except Exception:
-            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+            bot.send_message(message.from_user.id, 'Use numbers')
 
 
 def yesButton(message):
     print(message.message_id)
-    #bot.delete_message(message.chat.id, message.message_id - 1)
-    #bot.delete_message(message.chat.id, message.message_id - 2)
     bot.delete_message(message.chat.id, message.message_id)
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBackToXLM')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
     )
     bot.send_message(
         message.chat.id,
-        'Введите номер карты\n' +
-        'Формат: XXXX XXXX XXXX XXXX',
+        'Enter card number\n' +
+        'Like here: XXXX XXXX XXXX XXXX',
         reply_markup=keyboard
     )
 
     bot.register_next_step_handler(message, valueCardNumber)
+
+def yesButtonPP(message):
+    print(message.message_id)
+    bot.delete_message(message.chat.id, message.message_id)
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBackToXLM')
+    )
+    bot.send_message(
+        message.chat.id,
+        'Enter your PayPal mail:',
+        reply_markup=keyboard
+    )
+
+    bot.register_next_step_handler(message, userDataPP)
 
 
 def valueCardNumber(message):
@@ -235,10 +320,12 @@ def valueCardNumber(message):
     global card_number
     card_number = message.text
     if len(message.text) == 19:
-        bot.send_message(message.from_user.id, "Введите реквизиты держателя карты. Пример: Иванов Иван.")
+        bot.send_message(message.from_user.id, "Enter the details of the card holder. Example: Ivanov Ivan.")
         bot.register_next_step_handler(message, userData)
     else:
         yesButton(message)
+
+
 
 
 def userData(message):
@@ -248,19 +335,37 @@ def userData(message):
     user_data = message.text
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Подтвердить', callback_data='confirm')
+        telebot.types.InlineKeyboardButton('Confirm', callback_data='confirm')
     )
     bot.send_message(
         message.chat.id,
-        'Проверьте правильность введения данных:\n' +
-        'Номер карты: ' + card_number + '\n' +
-        'Имя держателя: ' + message.text + '\n' +
-        'Отдаёте:' + str(price) + '\n',
+        'Check the data entry is correct:\n' +
+        'Card number: ' + card_number + '\n' +
+        'Holder name: ' + message.text + '\n' +
+        'Give:' + str(price) + '\n',
         reply_markup=keyboard
     )
-    print('Номер карты: ' + card_number + '\n')
-    print('Имя держателя: ' + message.text + '\n')
-    print('Отдаёте:' + str(price) + '\n')
+    print('Card number: ' + card_number + '\n')
+    print('Holder name: ' + message.text + '\n')
+    print('Give:' + str(price) + '\n')
+
+def userDataPP(message):
+    pp_link = message.text
+    print(message.message_id)
+    bot.delete_message(message.chat.id, message.message_id - 1)
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Confirm', callback_data='confirm')
+    )
+    bot.send_message(
+        message.chat.id,
+        'Check the data entry is correct:\n' +
+        'PayPal: ' + pp_link + '\n' +
+        'Give: ' + str(price) + '\n',
+        reply_markup=keyboard
+    )
+    print('PayPal: ' + pp_link + '\n')
+    print('Give:' + str(price) + '\n')
 
 
 def confirm(message):
@@ -268,13 +373,13 @@ def confirm(message):
     bot.delete_message(message.chat.id, message.message_id)
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Выполнил', callback_data='gotowo')
+        telebot.types.InlineKeyboardButton('Performed', callback_data='gotowo')
     )
     bot.send_message(
         message.chat.id,
-        'Переведите точную суму: ' + str(price) + ' XLM на кошелек:\n' +
+        'Transfer the exact amount: ' + str(price) + ' XLM to wallet:\n' +
         'GB4JY7XWDUZAVFEZ4PURL42VTYOWGLUQ2R3RBNNLRD3BR7W276ZTJGEN\n' +
-        '№ вашей заявки ' + str(message.message_id),
+        'Your exchange request number:' + str(message.message_id),
         reply_markup=keyboard
     )
 
@@ -285,11 +390,11 @@ def finish(message):
     bot.delete_message(message.chat.id, message.message_id)
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        telebot.types.InlineKeyboardButton('Назад', callback_data='getBack')
+        telebot.types.InlineKeyboardButton('Back', callback_data='getBack')
     )
     bot.send_message(
         message.chat.id,
-        'После подтверждения оператором Вашего перевода средства будут зачислены в течении 15-20 минут.', reply_markup=keyboard
+        'After confirmation by the operator of your transfer, the funds will be credited within 15-20 minutes.', reply_markup=keyboard
     )
 
 
